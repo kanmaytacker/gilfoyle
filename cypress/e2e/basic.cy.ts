4962f4392e35a948b9ae296b664a0848aec9654d
Tanmay <tanmaykacker40@gmail.com>
2022-11-25 17:30:19 +0000

    Removes test.

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -3,7 +3,4 @@ describe('Basic specs', () => {
     cy.visit('/')
   })
 
-  it('Check for jokes link', () => {
-    cy.get('.jokes-link').should('have.attr', 'href').and('include', 'api/joke')
-  })
 })
